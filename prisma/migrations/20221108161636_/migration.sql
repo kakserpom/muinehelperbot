50d098dcd1dd51d90565064074169bb26628d45b
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2022-11-14 11:51:23 +0700

    Migration

--- prisma/migrations/20221108161636_/migration.sql
+++
@@ -1,3 +0,0 @@
--- This is an empty migration.
-
-CREATE INDEX qa_idx ON qa USING GIN (to_tsvector('russian', keywords));
--- prisma/migrations/20221114044855_/migration.sql
+++ prisma/migrations/20221114044855_/migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE "qa" (
     "id" SERIAL NOT NULL,
     "keywords" TEXT NOT NULL,
     "header" TEXT NOT NULL,
-    "links" TEXT NOT NULL,
+    "link" TEXT NOT NULL,
 
     CONSTRAINT "qa_pkey" PRIMARY KEY ("id")
 );
